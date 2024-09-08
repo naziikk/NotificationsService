748ddd328d91fde0d09d7011d3bd304e98feba33
Zakrevski Nazar <zacnazar20@gmail.com>
2024-09-08 12:26:07 +0300

    Добавлен класс обработки http запросов

---
+++ backend/request_handler/RequestHandler.h
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "../../libraries/httplib.h"
+#include <thread>
+#include "../multithread_notification_scheduler/Time_scheduler.h"
+#include "../../libraries/nlohmann/json.hpp"
+#include <unordered_map>
+#include <fstream>
+#include <regex>
+#include "backend/requestParser.h"
+
+
+class RequestHandler {
+public:
+    Time_scheduler scheduler;
+
+    void HttpPostToken(const httplib::Request& request, httplib::Response &res);
+
+    void HttpNotificationDelete(const httplib::Request& request, httplib::Response &res);
+
+    void HttpNotificationPutById(const httplib::Request& request, httplib::Response &res);
+
+    void HttpNotificationPost(const httplib::Request& request, httplib::Response &res, int id);
+
+    void HttpRegisterPost(const httplib::Request& request, httplib::Response &res);
+
+    bool isValidEmail(const std::string& email);
+
+    bool validateToken(const std::string& token);
+
+    std::string generateAuthToken();
+};
+
